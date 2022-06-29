6830a32f9cbb88f26a0024dd3e2541cedbb33cf1
yoviarpauzi <yoviarpauzi99@gmail.com>
2022-06-30 05:48:55 +0700

    add new commit

---
+++ 2. Ekspresi-dan-masukan-atau-keluaran/displaykalimat.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+using namespace std;
+int main(){
+    char kalimat[100];
+    scanf("%[^\n]s", &kalimat);
+    cout << kalimat;
+    return 0;
+}
---
+++ 2. Ekspresi-dan-masukan-atau-keluaran/luas-segitiga.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+int main(){
+    int A, T;
+    float hasil;
+    cin >> A >> T;
+    hasil = (1.0/2.0) * A * T;
+    printf("%.2f", hasil);
+    return 0;
+}
